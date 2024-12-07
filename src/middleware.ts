a43e764386acb807992e77a10d8c85e13cbbd6c1
11aghayan <11.aghayan.99@gmail.com>
2024-12-08 00:26:51 +0400

    middleware.ts added

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const token = req.cookies.get("jwt_token");
+  const url = req.nextUrl.clone();
+  const { pathname, searchParams } = url;
+  
+  const excluded_paths = [
+    '/_next',
+    '/_vercel',
+    '/api',
+    '/favicon.ico'
+  ];
+
+  if (excluded_paths.some(path => pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+  
+  const path_from = searchParams.get("pathfrom") ?? "";
+  
+  if (!token && !pathname.startsWith("/login")) {
+    url.pathname = '/login';
+    url.searchParams.set("pathfrom", pathname.slice(1));
+    return NextResponse.redirect(url);
+  }
+
+  if (token && pathname.startsWith("/login")) {
+    url.pathname = `/${path_from}`;
+    return NextResponse.redirect(url);
+  }
+  
+  return NextResponse.next(); 
+}
